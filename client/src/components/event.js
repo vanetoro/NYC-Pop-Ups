e519cec4a2507d1ae633b721c290f02a9e276e23
Vanessa Toro <vanetoro84@gmail.com>
2018-09-26 19:40:46 -0400

    added moment for date format

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Moment from 'react-moment';
+
 
 function Event(props) {
     return (
         <div>
             <h3>{props.event.name}</h3> 
             <p>Address: {props.event.location} </p>
-            <p>Start Date: {props.event.start_date}</p> 
-            <p>End Date: {props.event.end_date}</p> 
+            <p>Start Date: <Moment format='MMMM d YYYY'>{props.event.start_date}</Moment></p> 
+            <p>End Date: <Moment format='MMMM d YYYY'>{props.event.end_date}</Moment></p> 
         </div>
     )
 }
