d0fc351144b99048c2a9310cf54af83389613602
Vanessa Toro <vanetoro84@gmail.com>
2018-09-26 19:05:48 -0400

    backing up

--- client/src/actions/fetchEvents.js
+++ client/src/actions/fetchEvents.js
@@ -1,6 +1,7 @@
 export default function fetchEvents() {
     return(dispatch) => {
-    window.fetch('/api/events')
+        dispatch({type: 'LOADING'})
+        return window.fetch('/api/events')
         .then(response => response.json())
         .then(data => dispatch({type: 'FETCH_EVENTS', data}))
     }
