244cf384ebe9cf4183a69682cdd09ae62ee251c4
Vanessa Toro <vanetoro84@gmail.com>
2018-09-26 20:32:34 -0400

    added bootstap styling

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -4,11 +4,14 @@ import Moment from 'react-moment';
 
 function Event(props) {
     return (
-        <div>
-            <h3>{props.event.name}</h3> 
+        <div className="col-sm-8">
+        <div className='card' style={{width: "19rem"}}>
+            <h4 className="card-header">{props.event.name}</h4> 
             <p>Address: {props.event.location} </p>
-            <p>Start Date: <Moment format='MMMM d YYYY'>{props.event.start_date}</Moment></p> 
-            <p>End Date: <Moment format='MMMM d YYYY'>{props.event.end_date}</Moment></p> 
+            <p>Start Date: <Moment format='MMMM dd YYYY'>{props.event.start_date}</Moment></p> 
+            <p>End Date: <Moment format='MMMM dd YYYY'>{props.event.end_date}</Moment></p> 
+            <a href="#" class="btn btn-primary">See More</a>
+        </div>
         </div>
     )
 }
