8626bf16b55ed1fe46903dc45638a7f52eb003ce
Vanessa Toro <vanetoro84@gmail.com>
2018-09-27 19:14:58 -0400

    boostrap styling

--- client/src/components/newEvent.js
+++ client/src/components/newEvent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class NewEvent extends Component {
+class NewEvent extends React.Component {
 
     state = {
         name: '',
@@ -24,15 +24,48 @@ export default class NewEvent extends Component {
     render() {
         console.log(this.state)
         return (
+            <div className="container">
             <div className='row'>
-                <form className='form-group'  onSumbit={this.handleSubmit}>
-                   Name: <input className='form-control' type='text' name='name' onChange={this.handleChange.bind(this)}/>
-                   Location:<input className='form-control' type="text" name='location' onChange={this.handleChange.bind(this)}/>
-                   Start Date: <input className='form-control' type='date' name= 'startDate' onChange={this.handleChange.bind(this)}/>
-                   End Date: <input className='form-control' type="date" name= 'endDate' onChange={this.handleChange.bind(this)}/>
+                <form className='form' onSumbit={this.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input className='form-control' 
+                        type='text' 
+                        name='name' 
+                        id='name' 
+                        onChange={this.handleChange.bind(this)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="location">Location</label>
+                    <input className='form-control' 
+                        type="text" 
+                        name='location' 
+                        id='location' 
+                        onChange={this.handleChange.bind(this)}/>
+                </div>
+                    <div className="form-group">
+                    <label htmlFor="startDate">Start Date</label>
+                    <input className='form-control' 
+                        type='date' 
+                        name= 'startDate' 
+                        id= 'startDate' 
+                        onChange={this.handleChange.bind(this)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="endDate">End Date</label>
+                    <input className='form-control' 
+                        type="date" 
+                        name= 'endDate' 
+                        id= 'endDate' 
+                        onChange={this.handleChange.bind(this)}/>
+                </div>   
                    <input type='submit'/>
                 </form>
             </div>
+        </div>
         )
     }
 }
+
+
+export default NewEvent;
