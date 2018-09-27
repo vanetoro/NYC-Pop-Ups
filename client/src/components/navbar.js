700422ec42f44f3aedef592a8b72f5a940c64d72
Vanessa Toro <vanetoro84@gmail.com>
2018-09-26 20:33:15 -0400

    links and styling

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 function NavBar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">Events</a>
+        </nav>
     )
 }
 
