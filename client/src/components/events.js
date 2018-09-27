9104e0b39776c9174242e53c48412161fc19e6ec
Vanessa Toro <vanetoro84@gmail.com>
2018-09-27 19:16:25 -0400

    added styling

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Event from './event'
 
 function Events(props) {
-    let event = props.events.map(event => <div key={event.id}><Event event={event}/></div>)
+    let event = props.events.map(event => <div className='col-4' key={event.id}><Event event={event}/></div>)
     return (
-        <div className='row'>
-            {event}
-        </div>
+        <div className="container ">
+            <div className='row'>
+                <h2 className='mr-auto'>Current Pop-Ups</h2>
+            </div>   
+            <div className="row">
+                {event}
+            </div> 
+        </div>   
     )
 }
 
