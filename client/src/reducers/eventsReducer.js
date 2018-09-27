6f65a6450fbf3e5633018e585217127b55c3cae7
Vanessa Toro <vanetoro84@gmail.com>
2018-09-27 19:15:31 -0400

    added fetch_event case

--- client/src/reducers/eventsReducer.js
+++ client/src/reducers/eventsReducer.js
@@ -4,6 +4,8 @@ export default function eventsReducer(state = {
     switch (action.type) {
         case 'FETCH_EVENTS':
             return {...state, events: action.data}
+        case 'FETCH_EVENT':
+            return{...state, events: action.data}
         default:
             return state;
     }
