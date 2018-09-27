34a1f9f69fc5828a729851ae3e82023bf7d5edf2
Vanessa Toro <vanetoro84@gmail.com>
2018-09-27 19:03:24 -0400

    imported NavLink

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
     return (
+    
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-link" href="#">Events</a>
+            <NavLink to='/' extact className="nav-link" >Home <span class="sr-only">(current)</span></NavLink>
+            <NavLink to='/events/:id' className="nav-link" href="#">All Pop-Ups</NavLink>
+            <NavLink to='/events/new/' exact className="nav-link" href="#">Enter New Pop-Up</NavLink>
+
         </nav>
     )
 }
