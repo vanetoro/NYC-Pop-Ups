f9cfdf9e635854cf9a044910fbb9c7c4248271e3
Vanessa Toro <vanetoro84@gmail.com>
2018-09-27 19:16:55 -0400

    removed start and end date

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -4,12 +4,12 @@ import Moment from 'react-moment';
 
 function Event(props) {
     return (
-        <div className="col-sm-8">
+        <div className="col-8">
         <div className='card' style={{width: "19rem"}}>
             <h4 className="card-header">{props.event.name}</h4> 
             <p>Address: {props.event.location} </p>
-            <p>Start Date: <Moment format='MMMM dd YYYY'>{props.event.start_date}</Moment></p> 
-            <p>End Date: <Moment format='MMMM dd YYYY'>{props.event.end_date}</Moment></p> 
+            {/* <p>Start Date: <Moment format='MMMM dd YYYY'>{props.event.start_date}</Moment></p>  */}
+            {/* <p>End Date: <Moment format='MMMM dd YYYY'>{props.event.end_date}</Moment></p>  */}
             <a href="#" class="btn btn-primary">See More</a>
         </div>
         </div>
