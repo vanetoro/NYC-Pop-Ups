73320bacb384277836118c858e75f076348c5a3c
Vanessa Toro <vanetoro84@gmail.com>
2018-09-28 19:52:10 -0400

    ADD_EVENT case

--- client/src/reducers/eventsReducer.js
+++ client/src/reducers/eventsReducer.js
@@ -1,11 +1,14 @@
 export default function eventsReducer(state = {
-    events:  []
+    events:  [], 
+    show: false
 }, action) {
     switch (action.type) {
         case 'FETCH_EVENTS':
             return {...state, events: action.data}
         case 'FETCH_EVENT':
             return{...state, events: action.data}
+        case 'ADD_EVENT':
+            return{...state, show: true}    
         default:
             return state;
     }
