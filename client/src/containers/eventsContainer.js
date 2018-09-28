0f309ec0081cb868e4dbd96d3e007e3a2b5f3603
Vanessa Toro <vanetoro84@gmail.com>
2018-09-28 19:51:40 -0400

    added postEvent & addEvent to dispatch

--- client/src/containers/eventsContainer.js
+++ client/src/containers/eventsContainer.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react'
 import Events from '../components/events';
 import { connect } from 'react-redux'
-import fetchEvents, { fetchEvent } from '../actions/fetchEvents'
+import fetchEvents, { fetchEvent, postEvent } from '../actions/eventActions'
 import NewEvent from '../components/newEvent';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ShowEvent from '../components/showEvent';
-
+import { Button } from 'react-bootstrap'
+import '../eventsContainer.css'
 
 
  class EventsContainer extends Component {
-
+    
     componentDidMount () {
         this.props.getEvents()
+    }
+
+    handleClick(e){
+        e.preventDefault()
+        this.props.addEvent()
     }
     
 
     render() {
+
         return (
         <div> 
             <Router>
                 <React.Fragment>
                     <Route exact path='/' render={()=> <Events events={this.props.events}/>} />
-                    <Route exact path='/events/new' component={NewEvent} />
                     <Route exact path='/events/:id' component={ShowEvent} />
                 </React.Fragment>
             </Router>
-            
+            <Button bsStyle="primary" onClick={this.handleClick.bind(this)}>Add Event</Button>
+            <NewEvent show={this.props.show} postEvent={this.props.postEvent}/>
         </div>
         )
     }
@@ -33,14 +40,17 @@ import ShowEvent from '../components/showEvent';
 
     const mapStateToProps = (state) => {
         return {
-            events: state.events
+            events: state.events,
+            show: state.show
         }
     }
 
     const mapDispatchToProps = dispatch => {
         return{
             getEvents: () => dispatch(fetchEvents()),
-            getEvent: (id) => dispatch(fetchEvent(id))
+            getEvent: (id) => dispatch(fetchEvent(id)),
+            addEvent: () => dispatch({type: 'ADD_EVENT'}),
+            postEvent: (state) => dispatch(postEvent(state))
         }
     }
 
