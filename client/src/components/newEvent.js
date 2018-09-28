b68787a40d8f94c52edb447cad775c4b219d3ace
Vanessa Toro <vanetoro84@gmail.com>
2018-09-28 19:50:21 -0400

    modal is working

--- client/src/components/newEvent.js
+++ client/src/components/newEvent.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Modal, Button, FormControl } from 'react-bootstrap';
+import '../eventsContainer.css'
 
 class NewEvent extends React.Component {
 
@@ -17,55 +19,64 @@ class NewEvent extends React.Component {
         })
     }
 
+    handleClick(e){
+        debugger
+    }
+
     handleSubmit(e){
         e.preventDefault()
+        this.props.postEvent(this.state)
     }
 
     render() {
         console.log(this.state)
         return (
-            <div className="container">
-            <div className='row'>
-                <form className='form' onSumbit={this.handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input className='form-control' 
-                        type='text' 
-                        name='name' 
-                        id='name' 
-                        onChange={this.handleChange.bind(this)}/>
+            <Modal show={this.props.show}>
+                <div className="modal-container">
+                    <Modal.Header>
+                        <Modal.Title>Create New Event</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <form >
+                            <label htmlFor="name">Name</label>
+                            <FormControl 
+                                type='text' 
+                                name='name' 
+                                id='name' 
+                                onChange={this.handleChange.bind(this)}/>
+                            <label htmlFor="location">Location</label>
+                            <FormControl 
+                                type="text" 
+                                name='location' 
+                                id='location' 
+                                onChange={this.handleChange.bind(this)}/>
+                            <label htmlFor="startDate">Start Date</label>
+                            <FormControl 
+                                type='date' 
+                                name= 'startDate' 
+                                id= 'startDate' 
+                                onChange={this.handleChange.bind(this)}/>
+                            <label htmlFor="endDate">End Date</label>
+                            <FormControl 
+                                type="date" 
+                                name= 'endDate' 
+                                id= 'endDate' 
+                                onChange={this.handleChange.bind(this)}/>
+                            </form>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                    <Button onClick={this.handleClick.bind(this)}>Cancel</Button>
+                    <Button bsStyle="primary" onClick={this.handleSubmit.bind(this)}>Save</Button>
+                    </Modal.Footer>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="location">Location</label>
-                    <input className='form-control' 
-                        type="text" 
-                        name='location' 
-                        id='location' 
-                        onChange={this.handleChange.bind(this)}/>
-                </div>
-                    <div className="form-group">
-                    <label htmlFor="startDate">Start Date</label>
-                    <input className='form-control' 
-                        type='date' 
-                        name= 'startDate' 
-                        id= 'startDate' 
-                        onChange={this.handleChange.bind(this)}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="endDate">End Date</label>
-                    <input className='form-control' 
-                        type="date" 
-                        name= 'endDate' 
-                        id= 'endDate' 
-                        onChange={this.handleChange.bind(this)}/>
-                </div>   
-                   <input type='submit'/>
-                </form>
-            </div>
-        </div>
+            </Modal>
         )
     }
 }
 
 
 export default NewEvent;
+
+
