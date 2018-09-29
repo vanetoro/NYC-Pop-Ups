c4169c367f710ab060c381a50124e7a26ecef55e
Vanessa Toro <vanetoro84@gmail.com>
2018-09-29 09:00:24 -0400

    route to get all hoods

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do 
-    # resources :neighborhoods
+    resources :neighborhoods, only: [:index]
     resources :events
   end 
 
