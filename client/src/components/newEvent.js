19106172941344589d813261a1c565973d04022a
Vanessa Toro <vanetoro84@gmail.com>
2018-09-30 16:39:24 -0400

    neighborhood dropdown working

--- client/src/components/newEvent.js
+++ client/src/components/newEvent.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Modal, Button, FormControl, DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
+import { Modal, Button, FormControl, DropdownButton, MenuItem, FormGroup } from 'react-bootstrap';
 import '../eventsContainer.css'
 
 class NewEvent extends React.Component {
 
+
     state = {
         name: '',
-        location: '',
+        address: '',
         startDate: '',
         endDate: '',
-
-    
+        neighborhood: {
+            name: 'Choose Neighborhood'
+       }
     }
 
     handleChange(e){
@@ -19,64 +21,68 @@ class NewEvent extends React.Component {
         })
     }
 
-    handleClick(e){
-        debugger
+    handleSelect(id){
+        let index = id - 1
+        this.setState({
+            neighborhood: this.props.neighborhoods[index]
+        })
     }
 
     handleSubmit(e){
         e.preventDefault()
         this.props.postEvent(this.state)
     }
-
+ 
 
     render() {
-        let hoods = this.props.hoods.map(hood => <MenuItem eventKey={hood.id}>{hood.name}</MenuItem>)
-        console.log(this)
+        let neighborhoods = this.props.neighborhoods.map(neighborhood => <MenuItem onSelect={this.handleSelect.bind(this)} eventKey={neighborhood.id}>{neighborhood.name}</MenuItem>)
+        console.log(this.state)
         return (
             <Modal show={this.props.show}>
                 <div className="modal-container">
                     <Modal.Header>
                         <Modal.Title>Create New Event</Modal.Title>
                     </Modal.Header>
-
-                    <Modal.Body>
-                        <form >
-                            <label htmlFor="name">Name</label>
-                            <FormControl 
-                                type='text' 
-                                name='name' 
-                                id='name' 
-                                onChange={this.handleChange.bind(this)}/>
-                            <label htmlFor="location">Location</label>
-                            <FormControl 
-                                type="text" 
-                                name='location' 
-                                id='location' 
-                                onChange={this.handleChange.bind(this)}/>
-                            <label htmlFor="startDate">Start Date</label>
-                            <FormControl 
-                                type='date' 
-                                name= 'startDate' 
-                                id= 'startDate' 
-                                onChange={this.handleChange.bind(this)}/>
-                            <label htmlFor="endDate">End Date</label>
-                            <FormControl 
-                                type="date" 
-                                name= 'endDate' 
-                                id= 'endDate' 
-                                onChange={this.handleChange.bind(this)}/>
-                                <ButtonToolbar>
-                                <DropdownButton eventKey={1} title='Choose a Neighborhood' id='dropDown'> 
-                                {hoods}
-                                </DropdownButton>
-                                </ButtonToolbar> 
-                            </form>
-                            
-                    </Modal.Body>
-
+                        <Modal.Body>
+                            <form >
+                                <label htmlFor="name">Name</label>
+                                <FormControl 
+                                    type='text' 
+                                    name='name' 
+                                    id='name' 
+                                    onChange={this.handleChange.bind(this)}/>
+                                <label htmlFor="address">Address</label>
+                                <FormControl 
+                                    type="text" 
+                                    name='address' 
+                                    id='address' 
+                                    onChange={this.handleChange.bind(this)}/>
+                                <label htmlFor="startDate">Start Date</label>
+                                <FormControl 
+                                    type='date' 
+                                    name= 'startDate' 
+                                    id= 'startDate' 
+                                    onChange={this.handleChange.bind(this)}/>
+                                <label htmlFor="endDate">End Date</label>
+                                <FormControl 
+                                    type="date" 
+                                    name= 'endDate' 
+                                    id= 'endDate' 
+                                    onChange={this.handleChange.bind(this)}/>
+                                    <br/>
+                                <FormGroup>    
+                                    <label htmlFor="neighborhoodDropDown">Neighborhood </label>
+                                    <br />
+                                    <DropdownButton
+                                    eventKey={1} title={this.state.neighborhood.name} id='neighborhoodDropDown' > 
+                                    {neighborhoods}
+                                    </DropdownButton>
+                                </FormGroup>    
+                                </form>
+                        </Modal.Body>
                     <Modal.Footer>
-                    <Button onClick={this.handleClick.bind(this)}>Cancel</Button>
-                    <Button bsStyle="primary" onClick={this.handleSubmit.bind(this)}>Save</Button>
+                        <Button onClick={() => this.props.showNewEventForm()}>Cancel</Button>
+                        <Button bsStyle="primary" onClick={this.handleSubmit.bind(this)}>Save</Button>
                     </Modal.Footer>
                 </div>
             </Modal>
