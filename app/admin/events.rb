9bc487345da18a752bb6fcdbcd42410a32c9bf22
Vanessa Toro <vanetoro84@gmail.com>
2018-09-30 16:38:01 -0400

    changed location to adress

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Event do
-    permit_params :name, :location , :start_date, :end_date, :price, :neighborhood_id
+    permit_params :name, :address , :start_date, :end_date, :price, :neighborhood_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
