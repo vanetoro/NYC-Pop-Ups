9d8a15cf521b0cd8eaeb503e0b7d58973e94d31d
Vanessa Toro <vanetoro84@gmail.com>
2018-10-04 17:15:22 -0400

    intial setup with dispatch and navbar child

---
+++ client/src/containers/navbarContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import NavBar from '../components/navbar';
+import { connect } from 'react-redux'
+import { fetchUpComingEvents, fetchCurrentEvents, fetchPastEvents } from '../actions/eventActions'
+
+
+class NavbarContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavBar getUpcoming={this.props.getUpcoming} 
+                        getCurrent={this.props.getCurrent}
+                        getPast={this.props.getPast}/>
+            </div>
+        )
+    }
+}
+
+const mapDispatchtoProps = (dispatch) => {
+    return {
+        getCurrent: () => dispatch(fetchCurrentEvents()),
+        getUpcoming: () => dispatch(fetchUpComingEvents()),
+        getPast: () => dispatch(fetchPastEvents())
+    }
+}
+
+export default connect(null, mapDispatchtoProps)(NavbarContainer)
