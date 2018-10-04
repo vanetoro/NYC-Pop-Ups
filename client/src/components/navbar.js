8b5e1eb55b77ccdac6d6c81ed958fd6a1c2c604b
Vanessa Toro <vanetoro84@gmail.com>
2018-10-04 17:14:19 -0400

    fixed routing issue on navlinks

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -9,10 +9,10 @@ function NavBar(props) {
             <Navbar.Brand>NYC Pop-Ups</Navbar.Brand>
             <Nav>
             <NavItem>
-                <NavLink to='events/current' onClick={() => props.getCurrent()}  className="nav-link" >CURRENT <span class="sr-only">(current)</span></NavLink>
+                <NavLink to='/events/current' onClick={() => props.getCurrent()}  className="nav-link" >CURRENT <span class="sr-only">(current)</span></NavLink>
             </NavItem>
             <NavItem>
-                <NavLink to='events/upcoming'  onClick={() => props.getUpcoming()} className="nav-link" href="#">UPCOMING</NavLink>
+                <NavLink to='/events/upcoming'  onClick={() => props.getUpcoming()} className="nav-link" href="#">UPCOMING</NavLink>
             </NavItem>
             <NavItem>
                 <NavLink to='/events/past' onClick={() => props.getPast()} className="nav-link" href="#">PAST</NavLink>
