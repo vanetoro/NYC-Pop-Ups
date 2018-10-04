391f55975e11937d943ed1ebecac29abf8696f23
Vanessa Toro <vanetoro84@gmail.com>
2018-10-04 15:20:03 -0400

    upcoming_events method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ApplicationRecord
     belongs_to :neighborhood
+
+
+    def self.upcoming_events
+        self.where('start_date > ?', Time.now)
+    end
+
+    def self.past_events
+        self.where('end_date < ?', Time.now)
+    end
 end
+
