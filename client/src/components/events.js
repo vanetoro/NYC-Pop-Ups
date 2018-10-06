a1d35adf502b4dbbc2377ad09aefab9270a7e64b
Vanessa Toro <vanetoro84@gmail.com>
2018-10-06 13:16:58 -0400

    rrefactoring divs

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Event from './event'
+import { Row } from 'react-bootstrap';
+
 
 function Events(props) {
-    let event = props.events.map(event => <div className='col-4' key={event.id}><Event event={event}/></div>)
+    console.log(props)
+    let event = props.events.map(event =>{
+            return  (
+                <div key={event.id}>
+                    <Event event={event} getEvent={props.getEvent}/>
+                </div>)})
     return (
         <div className="container ">
-            <div className='row'>
                 <h2 className='mr-auto'>{props.type} Pop-Ups</h2>
-            </div>   
-            <div className="row">
                 {event}
-            </div> 
         </div>   
     )
 }
