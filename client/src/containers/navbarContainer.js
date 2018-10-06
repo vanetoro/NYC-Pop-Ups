2d252cd307f50c7dc410704da77c84bfd178e5b9
Vanessa Toro <vanetoro84@gmail.com>
2018-10-06 13:20:52 -0400

    + handleClick & dispatch props

--- client/src/containers/navbarContainer.js
+++ client/src/containers/navbarContainer.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import NavBar from '../components/navbar';
 import { connect } from 'react-redux'
-import { fetchUpComingEvents, fetchCurrentEvents, fetchPastEvents } from '../actions/eventActions'
+import fetchEvents, { fetchUpComingEvents, fetchCurrentEvents, fetchPastEvents, getHoods } from '../actions/eventActions'
 import '../navbar.css'
 
 class NavbarContainer extends React.Component {
+
+    handleClick(e){
+        e.preventDefault()
+        this.props.getHoods()
+        this.props.showNewEventForm()
+    }
+
     render() {
         return (
             <div>
                 <NavBar getUpcoming={this.props.getUpcoming} 
                         getCurrent={this.props.getCurrent}
-                        getPast={this.props.getPast}/>
+                        getPast={this.props.getPast}
+                        newEvent={this.handleClick.bind(this)}
+                        getEvents={this.props.getEvents}
+                />
             </div>
         )
     }
@@ -20,7 +30,11 @@ const mapDispatchtoProps = (dispatch) => {
     return {
         getCurrent: () => dispatch(fetchCurrentEvents()),
         getUpcoming: () => dispatch(fetchUpComingEvents()),
-        getPast: () => dispatch(fetchPastEvents())
+        getPast: () => dispatch(fetchPastEvents()),
+        getHoods: () => dispatch(getHoods()),
+        showNewEventForm: () => dispatch({type: 'ADD_EVENT'}),
+        getEvents: () => dispatch(fetchEvents())
+
     }
 }
 
