8c2de82740c9eda5357aa14f964105db251ab438
Vanessa Toro <vanetoro84@gmail.com>
2018-10-08 13:49:29 -0400

    component works shows event details

--- client/src/components/showEvent.js
+++ client/src/components/showEvent.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Moment from 'react-moment';
 
-function ShowEvent() {
+function ShowEvent(props) {
+    console.log(props.event.neighborhood_id)
+    // let hoodId = parseInt(props.event.neighborhood_id) -1
+    // let hood = props.hoods[hoodId]
     return (
         <div>
-            This is the show event component
+            <h1>{props.event.name}</h1>
+            <p>Description</p>
+            <p>Address: {props.event.location} in </p>
+            <p>Start Date: <Moment format='MMMM DD YYYY'>{props.event.start_date}</Moment></p> 
+            <p>End Date: <Moment format='MMMM DD YYYY'>{props.event.end_date}</Moment></p> 
+            <p>Price: {props.event.price}</p>
         </div>
     )
 }
