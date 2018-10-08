780fda4d35fa19d041b44d2b5b9f74f8c3eb4ee7
Vanessa Toro <vanetoro84@gmail.com>
2018-10-08 13:50:04 -0400

    - handleClick, showEvent route working

--- client/src/containers/eventsContainer.js
+++ client/src/containers/eventsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Events from '../components/events';
 import { connect } from 'react-redux'
-import fetchEvents, { fetchEvent, postEvent } from '../actions/eventActions'
+import fetchEvents, { fetchEvent, postEvent, getHoods } from '../actions/eventActions'
 import NewEvent from '../components/newEvent';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ShowEvent from '../components/showEvent';
@@ -12,14 +12,14 @@ import '../eventsContainer.css'
     
     componentDidMount () {
         this.props.getEvents()
-
-    }
-
-    handleClick(e){
-        e.preventDefault()
         this.props.getHoods()
-        this.props.showNewEventForm()
+
     }
+
+    // handleClick(e){
+    //     e.preventDefault()
+    //     this.props.showNewEventForm()
+    // }
 
     // handleUpComing(e){
     //     e.preventDefault()
@@ -40,7 +40,7 @@ import '../eventsContainer.css'
                                                      type={this.props.type}
                                                      getEvent={this.props.getEvent}
                                                      />} />
-                    <Route path='events/:id' render={() =><ShowEvent />} />                                
+                    <Route exact path='/events/:id' render={() => <ShowEvent event={this.props.events} hoods={this.props.hoods}/>} />                                
                 </React.Fragment>
             </Router>
             <NewEvent
@@ -70,9 +70,9 @@ import '../eventsContainer.css'
             getEvents: () => dispatch(fetchEvents()),
             getEvent: (id) => dispatch(fetchEvent(id)),
             postEvent: (state) => dispatch(postEvent(state)),
-            showNewEventForm: () => dispatch({type: 'ADD_EVENT'})
+            showNewEventForm: () => dispatch({type: 'ADD_EVENT'}),
+            getHoods: () => dispatch(getHoods())
             // getEvent: (id) => dispatch(fetchEvent(id)),
-            // getHoods: () => dispatch(getHoods()),
             // getUpcoming: () => dispatch(fetchUpComingEvents())
         }
     }
