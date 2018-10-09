7cfa86edee52f81c186ad8050bcd4249493aedde
Vanessa Toro <vanetoro84@gmail.com>
2018-10-09 18:02:19 -0400

    router paths filters and id working

--- client/src/containers/eventsContainer.js
+++ client/src/containers/eventsContainer.js
@@ -1,81 +1,66 @@
 import React, { Component } from 'react'
-import Events from '../components/events';
+import Events from '../components/events'
 import { connect } from 'react-redux'
 import fetchEvents, { fetchEvent, postEvent, getHoods } from '../actions/eventActions'
-import NewEvent from '../components/newEvent';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ShowEvent from '../components/showEvent';
+import NewEvent from '../components/newEvent'
+import { Route, Switch, withRouter } from 'react-router-dom'
+import ShowEvent from '../components/showEvent'
 import '../eventsContainer.css'
 
+class EventsContainer extends Component {
+  componentDidMount() {
+    this.props.getEvents()
+    this.props.getHoods()
+  }
 
- class EventsContainer extends Component {
-    
-    componentDidMount () {
-        this.props.getEvents()
-        this.props.getHoods()
 
-    }
-
-    // handleClick(e){
-    //     e.preventDefault()
-    //     this.props.showNewEventForm()
-    // }
-
-    // handleUpComing(e){
-    //     e.preventDefault()
-    //     this.props.getUpcoming()
-    // }
-    
-
-    render() {
-
-        return (
-        <div id='eventsContainer'> 
-            <div className='ml-auto'>
-            </div>    
-            <Router>
-                <React.Fragment>
-                    <Route exact path='/' 
-                                render={()=> <Events events={this.props.events} 
-                                                     type={this.props.type}
-                                                     getEvent={this.props.getEvent}
-                                                     />} />
-                    <Route path='/events/:id' component={ShowEvent} />                                
-                </React.Fragment>
-            </Router>
-            <NewEvent
-                show={this.props.show} 
-                postEvent={this.props.postEvent} 
-                removeForm={this.props.showNewEventForm}
-                neighborhoods={this.props.hoods}
+  render() {
+    return (
+      <div id="eventsContainer">
+        <React.Fragment>
+          <Switch>
+            <Route exact path="/" render={() => <Events 
+                                                
+                                                type={this.props.type} 
+                                                getEvent={this.props.getEvent} />}
             />
-        </div>
-        )
-    }
+            <Route exact path="/:type" component={Events} events={this.props.events} />
+            <Route exact path="/events/:id" component={ShowEvent} />
+          </Switch>
+        </React.Fragment>
+        <NewEvent
+          show={this.props.show}
+          postEvent={this.props.postEvent}
+          removeForm={this.props.showNewEventForm}
+          neighborhoods={this.props.hoods}
+        />
+      </div>
+    )
+  }
 }
 
-    const mapStateToProps = (state) => {
-        // debugger
-        return {
-            events: state.events,
-            show: state.show,
-            hoods: state.hoods, 
-            type: this.type
+const mapStateToProps = (state) => {
+  return {
+    events: state.events,
+    show: state.show,
+    hoods: state.hoods,
+    type: this.type
+    // ownProps: ownProps
+  }
+}
 
-        }
-    }
+const mapDispatchToProps = dispatch => {
+  return {
+    getEvents: () => dispatch(fetchEvents()),
+    getEvent: id => dispatch(fetchEvent(id)),
+    postEvent: state => dispatch(postEvent(state)),
+    showNewEventForm: () => dispatch({ type: 'ADD_EVENT' }),
+    getHoods: () => dispatch(getHoods())
+    // getEvent: (id) => dispatch(fetchEvent(id)),
+    // getUpcoming: () => dispatch(fetchUpComingEvents())
+  }
+}
 
-    const mapDispatchToProps = dispatch => {
-        return{
-            getEvents: () => dispatch(fetchEvents()),
-            getEvent: (id) => dispatch(fetchEvent(id)),
-            postEvent: (state) => dispatch(postEvent(state)),
-            showNewEventForm: () => dispatch({type: 'ADD_EVENT'}),
-            getHoods: () => dispatch(getHoods())
-            // getEvent: (id) => dispatch(fetchEvent(id)),
-            // getUpcoming: () => dispatch(fetchUpComingEvents())
-        }
-    }
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(EventsContainer)
+export default withRouter(
+    connect(mapStateToProps, mapDispatchToProps)(EventsContainer)
+);
