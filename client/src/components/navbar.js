007bcc0ca5f01217cebf6f752dad9fbf80dd3751
Vanessa Toro <vanetoro84@gmail.com>
2018-10-09 18:01:11 -0400

    updated navbar to use router

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,29 +1,37 @@
 import React from 'react'
-import { Navbar, NavItem, Nav, Button } from 'react-bootstrap';
-import { Link, NavLink} from 'react-router-dom';
+import { Navbar, NavItem, Nav, Button } from 'react-bootstrap'
+import { Link, NavLink } from 'react-router-dom'
 
 function NavBar(props) {
-    console.log(props)
-    return (
-        <Navbar>
-            <Navbar.Brand onClick={() => props.getEvents()} >NYC Pop-Ups</Navbar.Brand>
-            <Nav>
-            <NavItem>
-                <Link to='/events/current'  className="nav-link" >CURRENT <span class="sr-only">(current)</span></Link>
-            </NavItem>
-            <NavItem>
-                <NavLink to='/events/upcoming'  onClick={() => props.getUpcoming()} className="nav-link" href="#">UPCOMING</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink to='/events/past' onClick={() => props.getPast()} className="nav-link" href="#">PAST</NavLink>
-            </NavItem>
-            <NavItem>
-            </NavItem>
-            </Nav>
-            <Button bsStyle="success" className='custom-btn' onClick={(e) => props.newEvent(e)} >Add Event</Button>
-
-        </Navbar>
-    )
+  console.log(props)
+  return (
+    <Navbar>
+      <Navbar.Brand>
+        <NavLink to="/">NYC Pop-Ups</NavLink>
+      </Navbar.Brand>
+      <Nav>
+        <NavItem>
+          <NavLink to="/current" className="nav-link">
+            CURRENT <span class="sr-only">(current)</span>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/upcoming" className="nav-link" href="#">
+            UPCOMING
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/past" className="nav-link" href="#">
+            PAST
+          </NavLink>
+        </NavItem>
+        <NavItem />
+      </Nav>
+      <Button bsStyle="success" className="custom-btn" onClick={e => props.newEvent(e)}>
+        Add Event
+      </Button>
+    </Navbar>
+  )
 }
 
 export default NavBar
