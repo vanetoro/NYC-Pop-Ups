927dc6b3f7449150f0a8cab35280846cbf09b680
Vanessa Toro <vanetoro84@gmail.com>
2018-10-09 18:01:44 -0400

    show event working properly

--- client/src/components/showEvent.js
+++ client/src/components/showEvent.js
@@ -1,25 +1,33 @@
 import React from 'react'
-import Moment from 'react-moment';
+import Moment from 'react-moment'
 import { connect } from 'react-redux'
 
 function ShowEvent(props) {
-    console.log(props)
-    // let hoodId = parseInt(props.event.neighborhood_id) -1
-    // let hood = props.hoods[hoodId]
-    return (
-        <div>
+  // let hoodId = parseInt(props.event.neighborhood_id) -1
+  // let hood = props.hoods[hoodId]
+//   console.log(props)
+  return (
+    <React.Fragment>
             <h1>{props.event.name}</h1>
             <p>Description</p>
             <p>Address: {props.event.location} in </p>
-            <p>Start Date: <Moment format='MMMM DD YYYY'>{props.event.start_date}</Moment></p> 
-            <p>End Date: <Moment format='MMMM DD YYYY'>{props.event.end_date}</Moment></p> 
+            <p>
+              Start Date: <Moment format="MMMM DD YYYY">{props.event.start_date}</Moment>
+            </p>
+            <p>
+              End Date: <Moment format="MMMM DD YYYY">{props.event.end_date}</Moment>
+            </p>
             <p>Price: {props.event.price}</p>
-        </div>
-    )
-}
+    </React.Fragment>
+  )
+}   
 
 const mapStateToProps = (state, ownProps) => {
-    debugger
+    const id = parseInt(ownProps.match.params.id)
+    let event = state.events.find((event) => event.id === id)
+  return {
+    event
+  }
 }
 
-export default connect(mapStateToProps)(ShowEvent)
+export default connect(mapStateToProps)(ShowEvent)  
