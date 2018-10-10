81aac647bf6ad473b850a7fd609fd6a2ba44b53e
Vanessa Toro <vanetoro84@gmail.com>
2018-10-09 20:36:30 -0400

    + description & address

--- db/migrate/20180925001451_create_events.rb
+++ db/migrate/20180925001451_create_events.rb
@@ -2,7 +2,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.string :location
+      t.string :description
+      t.string :address
       t.date :start_date
       t.date :end_date
       t.string :price
