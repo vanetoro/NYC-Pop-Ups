eed5243b4176032cf1e3d7933e3dcb6e7ba44f7e
Vanessa Toro <vanetoro84@gmail.com>
2018-10-09 20:36:04 -0400

    + description, image, price

--- client/src/components/newEvent.js
+++ client/src/components/newEvent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal, Button, FormControl, DropdownButton, MenuItem, FormGroup } from 'react-bootstrap';
+import { Modal, Button, FormControl, DropdownButton, MenuItem, FormGroup, ControlLabel } from 'react-bootstrap';
 import '../eventsContainer.css'
 
 class NewEvent extends React.Component {
@@ -7,9 +7,11 @@ class NewEvent extends React.Component {
 
     state = {
         name: '',
+        description: '',
         address: '',
-        startDate: '',
-        endDate: '',
+        start_date: '',
+        end_date: '',
+        avatar: '',
         neighborhood: {
             name: 'Choose Neighborhood'
        }
@@ -22,9 +24,8 @@ class NewEvent extends React.Component {
     }
 
     handleSelect(id){
-        let index = id - 1
         this.setState({
-            neighborhood: this.props.neighborhoods[index]
+            neighborhood: this.props.neighborhoods.find((hood) => hood.id === id)
         })
     }
 
@@ -35,8 +36,8 @@ class NewEvent extends React.Component {
  
 
     render() {
-        let neighborhoods = this.props.neighborhoods.map(neighborhood => <MenuItem onSelect={this.handleSelect.bind(this)} eventKey={neighborhood.id}>{neighborhood.name}</MenuItem>)
         console.log(this.state)
+        let neighborhoods = this.props.neighborhoods.map(neighborhood => <MenuItem onSelect={this.handleSelect.bind(this)} eventKey={neighborhood.id}>{neighborhood.name}</MenuItem>)
         return (
             <Modal show={this.props.show}>
                 <div className="modal-container">
@@ -45,44 +46,75 @@ class NewEvent extends React.Component {
                     </Modal.Header>
                         <Modal.Body>
                             <form >
+                                <FormGroup>
                                 <label htmlFor="name">Name</label>
                                 <FormControl 
                                     type='text' 
                                     name='name' 
                                     id='name' 
                                     onChange={this.handleChange.bind(this)}/>
+                                </FormGroup>        
+                                <FormGroup> 
+                                    <ControlLabel>Description</ControlLabel>
+                                    <FormControl componentClass="textarea" 
+                                    name= 'description'
+                                    id= 'description'
+                                    onChange={this.handleChange.bind(this)}/>
+                                </FormGroup>
+                                <FormGroup>
                                 <label htmlFor="address">Address</label>
                                 <FormControl 
                                     type="text" 
                                     name='address' 
                                     id='address' 
                                     onChange={this.handleChange.bind(this)}/>
+                                </FormGroup>   
+                                <FormGroup>
+                                <label htmlFor="price">Price</label>
+                                <FormControl 
+                                    type="text" 
+                                    name='price' 
+                                    id='price' 
+                                    onChange={this.handleChange.bind(this)}/>
+                                </FormGroup>   
                                 <label htmlFor="startDate">Start Date</label>
+                                <FormGroup>
                                 <FormControl 
                                     type='date' 
-                                    name= 'startDate' 
-                                    id= 'startDate' 
+                                    name= 'start_date' 
+                                    id= 'startDate'
                                     onChange={this.handleChange.bind(this)}/>
+                                </FormGroup>
+                                <FormGroup>    
                                 <label htmlFor="endDate">End Date</label>
                                 <FormControl 
                                     type="date" 
-                                    name= 'endDate' 
+                                    name= 'end_date' 
                                     id= 'endDate' 
                                     onChange={this.handleChange.bind(this)}/>
                                     <br/>
+                                </FormGroup>                                    
                                 <FormGroup>    
                                     <label htmlFor="neighborhoodDropDown">Neighborhood </label>
                                     <br />
                                     <DropdownButton
-                                    eventKey={1} title={this.state.neighborhood.name} id='neighborhoodDropDown' > 
+                                    eventKey={1} onSelect={this.handleSelect.bind(this)} title={this.state.neighborhood.name} id='neighborhoodDropDown' > 
                                     {neighborhoods}
                                     </DropdownButton>
                                 </FormGroup>    
+                                <FormControl
+                                    name="avatar"
+                                    type="file"
+                                    label="File"
+                                    help="Example block-level help text here."
+                                    onChange={this.handleChange.bind(this)}
+                                    />
                                 </form>
                         </Modal.Body>
                     <Modal.Footer>
                         <Button onClick={() => this.props.removeForm()}>Cancel</Button>
                         <Button bsStyle="primary" onClick={this.handleSubmit.bind(this)}>Save</Button>
+                        
                     </Modal.Footer>
                 </div>
             </Modal>
