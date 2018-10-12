44ce057640088341bf3c49f4157e142bd6c895f6
Vanessa Toro <vanetoro84@gmail.com>
2018-10-12 08:38:51 -0400

    + post, delete & edit events

--- client/src/reducers/eventsReducer.js
+++ client/src/reducers/eventsReducer.js
@@ -1,14 +1,20 @@
 export default function eventsReducer(state = {
-    type: 'All',
     events:  [], 
     hoods: [],
-    show: false
+    show: false,
+    editEvent: {}
 }, action) {
     switch (action.type) {
         case 'FETCH_EVENTS':
             return {...state, events: action.data}
         case 'ADD_EVENT':
             return{...state, show: !state.show} 
+        case 'POST_EVENT':
+            return{...state, events: [...state.events, action.data], show: !state.show}
+        case 'DELETE_EVENT':
+            return{...state.events.filter(event => event.id !== action.id)} 
+        case 'EDIT_EVENT':
+            return{...state, editEvent: action.state, show: !state.show} 
         case 'FETCH_HOODS':
         return {...state, hoods: action.data}   
         default:
