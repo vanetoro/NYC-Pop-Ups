52e3cd2ab4201a7110e66dc5150552c7026d2f4d
Vanessa Toro <vanetoro84@gmail.com>
2018-10-12 08:36:20 -0400

    image beign displayed

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Col } from 'react-bootstrap';
+import { Col, Image } from 'react-bootstrap';
 import { NavLink} from 'react-router-dom';
 
 function Event(props) {
 
     return (
             <Col xs={6} md={3}>
+                <Image src={props.event.imageUrl} rounded responsive/>
                 <h4 >{props.event.name}</h4> 
                 
                 <NavLink to={`/events/${props.event.id}`} className="nav-link" >See More</NavLink>
