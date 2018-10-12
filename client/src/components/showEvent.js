1228d308cc17814a5c18e8e0e6fecc037acc16cb
Vanessa Toro <vanetoro84@gmail.com>
2018-10-12 08:37:49 -0400

    + delete & edit buttons

--- client/src/components/showEvent.js
+++ client/src/components/showEvent.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import Moment from 'react-moment'
 import { connect } from 'react-redux'
+import { Button } from 'react-bootstrap';
+import { deleteEvent } from '../actions/eventActions'
+import { withRouter } from "react-router-dom";
+
+
 
 function ShowEvent(props) {
-  // let hoodId = parseInt(props.event.neighborhood_id) -1
-  // let hood = props.hoods[hoodId]
-  // console.log(props)
+  
+  const handleclick= (e) => {
+      e.preventDefault()
+      props.editForm(props.event)
+      
+    }
+
+  const handleDelete = (e) => {
+    e.preventDefault()
+    props.deleteEvent(props.event.id)
+    props.history.push('/')
+
+  }  
   return (
     <React.Fragment>
-            <img src={props.event.imageUrl}/>
             <h1>{props.event.name}</h1>
             <p>{props.event.description}</p>
-            <p>Address: {props.event.address} in </p>
+            <p>Address: {props.event.address} </p>
             <p>
               Start Date: <Moment format="MMMM DD YYYY">{props.event.start_date}</Moment>
             </p>
@@ -19,9 +33,19 @@ function ShowEvent(props) {
               End Date: <Moment format="MMMM DD YYYY">{props.event.end_date}</Moment>
             </p>
             <p>Price: {props.event.price}</p>
+            <Button bsStyle='info' onClick={handleclick}>Edit</Button>
+            <Button bsStyle="danger" onClick={handleDelete}>Delete</Button>
     </React.Fragment>
   )
 }   
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    editForm: (state) => dispatch({ state, type: 'EDIT_EVENT' }),
+    deleteEvent: (id) => dispatch(deleteEvent(id))
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
     const id = parseInt(ownProps.match.params.id)
@@ -31,4 +55,4 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(ShowEvent)  
+export default withRouter( connect(mapStateToProps, mapDispatchToProps)(ShowEvent) )
