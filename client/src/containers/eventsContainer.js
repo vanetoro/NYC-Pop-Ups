a9e2f80b5d1beec3fbcb9321e8a7c4410d006391
Vanessa Toro <vanetoro84@gmail.com>
2018-10-13 17:14:41 -0400

    removed unused methods

--- client/src/containers/eventsContainer.js
+++ client/src/containers/eventsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Events from '../components/events'
 import { connect } from 'react-redux'
-import fetchEvents, { fetchEvent, postEvent, getHoods } from '../actions/eventActions'
+import fetchEvents, { fetchEvent, postEvent, getHoods, updateEvent } from '../actions/eventActions'
 import NewEvent from '../components/newEvent'
 import { Route, Switch, withRouter } from 'react-router-dom'
 import ShowEvent from '../components/showEvent'
@@ -19,11 +19,7 @@ class EventsContainer extends Component {
       <div id="eventsContainer">
         <React.Fragment>
           <Switch>
-            <Route exact path="/" render={() => <Events 
-                                                
-                                                type={this.props.type} 
-                                                getEvent={this.props.getEvent} />}
-            />
+            <Route exact path="/" render={() => <Events getEvent={this.props.getEvent} />} />
             <Route exact path="/:type" component={Events} events={this.props.events} />
             <Route exact path="/events/:id" component={ShowEvent} />
           </Switch>
@@ -31,21 +27,23 @@ class EventsContainer extends Component {
         <NewEvent
           show={this.props.show}
           postEvent={this.props.postEvent}
+          updateEvent={this.props.updateEvent}
           removeForm={this.props.showNewEventForm}
           neighborhoods={this.props.hoods}
+          edit={this.props.edit}
         />
       </div>
     )
   }
 }
 
+
 const mapStateToProps = (state) => {
   return {
     events: state.events,
     show: state.show,
     hoods: state.hoods,
-    type: this.type
-    // ownProps: ownProps
+    edit: state.editEvent
   }
 }
 
@@ -55,9 +53,8 @@ const mapDispatchToProps = dispatch => {
     getEvent: id => dispatch(fetchEvent(id)),
     postEvent: state => dispatch(postEvent(state)),
     showNewEventForm: () => dispatch({ type: 'ADD_EVENT' }),
-    getHoods: () => dispatch(getHoods())
-    // getEvent: (id) => dispatch(fetchEvent(id)),
-    // getUpcoming: () => dispatch(fetchUpComingEvents())
+    getHoods: () => dispatch(getHoods()),
+    updateEvent: (state, id) => dispatch(updateEvent(state, id))
   }
 }
 
