1d2a8975577232a3fdd44392839f935c8c552f7f
Vanessa Toro <vanetoro84@gmail.com>
2018-10-14 13:21:18 -0400

    styling to image so that are same size and image centered

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -6,10 +6,12 @@ function Event(props) {
 
     return (
             <Col xs={6} md={3}>
-                <NavLink to={`/events/${props.event.id}`} className="nav-link" >
-                    <Image src={props.event.imageUrl} rounded responsive/>
-                    <h4 >{props.event.name}</h4> 
-                </NavLink>
+                    <NavLink to={`/events/${props.event.id}`} className="nav-link" >
+                        <h4 >{props.event.name}</h4> 
+                <div style={{height: 175}}>
+                        <Image style={{height: 150, width: 'auto', marginLeft: 'auto', marginRight: 'auto'}} src={props.event.imageUrl} rounded responsive/>
+                </div>
+                    </NavLink>
             </Col>
     )
 }
