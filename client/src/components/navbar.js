16710953774ee42da49cfbae55d5715a7efe6883
Vanessa Toro <vanetoro84@gmail.com>
2018-10-16 10:24:18 -0400

    removed unused Link from import

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, NavItem, Nav, Button } from 'react-bootstrap'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function NavBar(props) {
   // console.log(props)
