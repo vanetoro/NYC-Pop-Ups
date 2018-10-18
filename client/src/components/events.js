d1fc24abb7bc98ad25a30cbc8018b81aca7e4ada
Vanessa Toro <vanetoro84@gmail.com>
2018-10-18 18:32:19 -0400

    changed to stateful component for sort feature

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -1,49 +1,83 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Event from './event'
 import { connect } from 'react-redux'
+import { Button } from 'react-bootstrap';
 
 
-function Events(props) {
-    if (!props.events) {
-        return null
-    }
+class Events extends Component  {
 
-    let event = props.events.map(event =>{
-            return  (
-                <div key={event.id}>
-                    <Event event={event} getEvent={props.getEvent}/>
-                </div>)})
-    return (
-        <div className="container-fluid">
-                <h2 className='mr-auto'>{props.type} Pop-Ups</h2>
-                {event}
-        </div>   
-    )
+    // if (!props.events) {
+    //     return null
+    // }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      events: nextProps.events
+    })
+  }
+  
+  // handleClickRecentlyAdded(e){
+  //   e.preventDefault()
+  //   let recents = this.state.events.sort((a,b) => b.id - a.id).slice(0,3)
+  //   this.setState({
+  //     events: recents
+  //   })
+  // }
+  
+  handleClickUp(e){
+    e.preventDefault()
+    this.setState({
+      events: this.state.events.sort((a,b) => b.counter - a.counter) 
+    })
+  }
+
+  handleClickDown(e){
+    e.preventDefault()
+    this.setState({
+      events: this.state.events.sort((a,b) => a.counter - b.counter) 
+    })
+  }
+   
+     render(){
+      let event = this.props.events.map(event =>{
+              return  (
+                  <div key={event.id}>
+                      <Event event={event} getEvent={this.props.getEvent} counter={this.props.counter}/>
+                  </div>)})
+      return (
+          <div className="container-fluid">
+           <Button onClick={this.handleClickUp.bind(this)}>⇡</Button>
+           <Button onClick={this.handleClickDown.bind(this)}>⇣</Button>
+                  <h2 className='mr-auto'>{this.props.type} Pop-Ups</h2>
+                  {event}
+          </div>   
+      )
+  }
 }
 
 
-
 const mapStateToProps = (state, ownProps) => {
+  // debugger
+    console.log(ownProps, 'inside mapStateToProps')
     let events;
-    
     const filter = ownProps.match && ownProps.match.params.type
-    if (filter == 'current') {
+    if (filter === 'current') {
       //filter the events that have a start date before today and an end date after today
-      events = state.events.reverse().filter(event => {
+      events = state.events.filter(event => {
         return Date.parse(event.start_date) <= Date.now() && Date.parse(event.end_date) >= Date.now()
       })
-    } else if (filter == 'upcoming') {
+    } else if (filter === 'upcoming') {
       //filter the events that have a start date and an end date after today
-      events = state.events.reverse().filter(event => {
+      events = state.events.filter(event => {
         return Date.parse(event.end_date) >= Date.now() && Date.parse(event.start_date) >= Date.now()
       })
-    } else if(filter == "past") {
+    } else if(filter === "past") {
       //filter the events that have a start date and an end date before today
-      events = state.events.reverse().filter(event => {
+      events = state.events.filter(event => {
         return Date.parse(event.end_date) <= Date.now() && Date.parse(event.start_date) <= Date.now()
       })
     } else {
-        events = state.events.reverse()
+        events = state.events
     }
     //pass the filtered events as props to the ShowEvent component
     return {
